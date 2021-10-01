8dce7351ec2e7a5479ba862a1e0064f83a52ad28
girevik1 <girevik1988@gmail.com>
2021-10-02 01:08:44 +0300

    added foto Mary and text about her

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="container">
     <Header />
+    <Content />
     <Carousel />
 </div>
 </template>
@@ -8,6 +9,7 @@
 <script>
 
 import Header from './components/Header.vue'
+import Content from './components/Content.vue'
 import Carousel from './components/Carousel.vue'
 
 export default {
@@ -20,6 +22,7 @@ export default {
 
     components: {
         Header,
+        Content,
         Carousel,
     }
 }
--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -29,7 +29,7 @@
 
 <script>
 export default {
-  name: 'Header',
+  name: 'Carousel',
   props: {},
     data() {
         return {
